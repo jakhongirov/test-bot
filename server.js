0aea6ec7fe8604904ec27a36919a86f63cb28392
jakhongirov <johongirovdiyorbek08@gmail.com>
2024-08-24 20:16:40 +0500

    add

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ async function getAIResponse(question) {
 }
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5060;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
